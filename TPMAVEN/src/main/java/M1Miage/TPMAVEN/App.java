104af655a0173f8f35f03f696dbac0e5d89b2b0c
alecreach <alexandre.creach@gmail.com>
2014-01-22 15:06:13 +0100

    test

---
+++ TPMAVEN/src/main/java/M1Miage/TPMAVEN/App.java
@@ -0,0 +1,14 @@
+package M1Miage.TPMAVEN;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+/**
+ * On fait de la javadoc
+ */
+System.out.println("Hello World!");
+}
+}
